2e54593d2c3f23574ce7a81541c6000550e427b2
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-09-08 23:18:43 +0200

    Add prop-types install

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,9 @@ class App extends React.Component {
       <main className ={styles.component}>
         <h1 className ={styles.title}>My first React app</h1>
         <h2 className ={styles.subtitle}>Hello world!</h2>
-        <List />
+        <List title={['Things to do', <sup key='1'>soon!</sup>]}>
+          <p>I'm planning on doing all these things sooner, rather than later!</p>
+        </List>
       </main>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static PropTypes = {
+    title: PropTypes.node,
+    children: PropTypes.node,
+  }
   render() {
     return (
       <section className ={styles.component}>
-       <Hero />
+       <Hero titleText={this.props.title} />
+       <div className={styles.description}>
+        {this.props.children}
+       </div>
       </section>
     )
   }
