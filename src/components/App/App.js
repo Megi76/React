23d599d86829341861395f8e55254804050481ee
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-09-26 20:56:36 +0200

    Add animation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 import FAQ from '../FAQ/FAQ';
+import {AnimatedSwitch} from 'react-router-transition';
+import styles from '../App/App.scss';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
+
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/FAQ' component={FAQ} />
-      </Switch>
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
